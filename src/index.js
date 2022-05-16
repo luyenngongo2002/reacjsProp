39f3f6420d58ded6cf86d1ae49d70e56440694ae
luyenngongo2002 <luyenngongo3112@gmail.com>
2022-05-16 11:50:11 +0700

    Form5

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import VdForm2 from './pages/component/FormVidu2';
-
+import FormVidu2 from './pages/component/FormVidu3';
+import Score from './pages/component/FormVidu4';
+import FormVidu5 from './pages/component/FormVidu4/index1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <VdForm2/>
+    <FormVidu5/>
   </React.StrictMode>
   //component
 );
