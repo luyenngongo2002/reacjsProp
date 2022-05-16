68195e49fc44e5c4246a20c311e5d0c64250adcf
luyenngongo2002 <luyenngongo3112@gmail.com>
2022-05-16 09:47:12 +0700

    Update FromVidu3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import ColorBox from './pages/component/StateVidu3/ColorBox';
-// import Counter from "./pages/component/StateVidu3/Counter";
-// import State from './pages/component/StateVidu2/state';
-// import Content from './pages/component/StateVidu1/StateVidu1';
 import reportWebVitals from './reportWebVitals';
 import VdForm2 from './pages/component/FormVidu2';
 
@@ -12,7 +8,7 @@ import VdForm2 from './pages/component/FormVidu2';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <VdForm2 />
+    <VdForm2/>
   </React.StrictMode>
   //component
 );
---
+++ src/pages/component/FormVidu3/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+class FormVidu2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form div="container"onSubmit={this.handleSubmit}>
+                <div c>
+                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <input type="text" class="form-control" value={this.state.value} onChange={this.handleChange}/>
+                </div>
+                
+                <div class="mb-3" value={this.state.value} onChange={this.handleChange}>
+                    <input type="radio" value="Male" name="gender" /> Male
+                    <input type="radio" value="Female" name="gender" /> Female
+                    <input type="radio" value="Other" name="gender" /> Other
+                </div>
+                <div class="mb-3">
+                    <input type="checkbox" id="topping" name="topping" value="Paneer" />Paneer
+                </div >
+                <div class="mb-3">
+                    <label>
+                        Pick your favorite flavor:
+                        <select value={this.state.value} onChange={this.handleChange}>
+                            <option value="grapefruit">Grapefruit</option>
+                            <option value="lime">Lime</option>
+                            <option value="coconut">Coconut</option>
+                            <option value="mango">Mango</option>
+                        </select>
+                    </label>
+                </div>
+               
+                <div class="mb-3">
+                    <label>
+                        Essay:
+                        <textarea value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            
+        );
+    }
+}
+export default FormVidu2;
